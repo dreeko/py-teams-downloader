6fa7d9fca4927d70bacce937275905b4b3a4c537
Keegan Ott <keegan.ott@dreeko.me>
2021-08-04 01:38:41 +0800

    initial UI impl

--- testing.py
+++ testing.py
@@ -10,7 +10,32 @@ import os
 import datetime
 import time
 import ui
+import tkinter as tk
 
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        scrollData = tk.StringVar()
+        self.hi_there = tk.Button(self)
+        self.chat_list = tk.Listbox(self, listvariable=scrollData)
+        for i in range(1,100):
+            self.chat_list.insert('end', i)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+        self.chat_list.pack(side="left")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
 # Save cookie
 
 
